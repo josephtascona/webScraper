88a69025fb46c5077a9b71ab8a05aaf1a31e0177
Joseph Tascona <jtascona@Josephs-MBP.hitronhub.home>
2022-05-23 19:59:34 -0400

    Finished scraper

--- scrapers.js
+++ scrapers.js
@@ -5,11 +5,23 @@ async function scrapeProduct(url) {
     const page = await browser.newPage();
     await page.goto(url);
 
+    await page.waitForXPath('//*[@id="imgBlkFront"]')
     const [el] = await page.$x('//*[@id="imgBlkFront"]');
     const src = el.getProperty('src');
-    const srcTxt = await src.jsonValue();
+    const imgURL = (await src).jsonValue;
 
-    console.log({srcTxt})
+    await page.waitForXPath('//*[@id="productTitle"]')
+    const [el2] = await page.$x('//*[@id="productTitle"]');
+    const txt = el2.getProperty('textContent');
+    const title = (await txt).jsonValue;
+
+    await page.waitForXPath('//*[@id="price"]')
+    const [el3] = await page.$x('//*[@id="price"]');
+    const txt2 = el3.getProperty('textContent');
+    const price = (await txt2).jsonValue;
+
+    console.log({imgURL, title, price});
+    await browser.close();
 }
 
-scrapeProduct('https://www.amazon.ca/Atomic-Habits-Proven-Build-Break/dp/0735211299/ref=sr_1_1?gclid=CjwKCAjw4ayUBhA4EiwATWyBrqMPyRNjdAsm6qFAgPdgg3VU3V-49gSmfUbboklXEv5-jiFJXEw5hhoCVLEQAvD_BwE&hvadid=538107724032&hvdev=c&hvlocphy=9000800&hvnetw=g&hvqmt=e&hvrand=14293299909348710143&hvtargid=kwd-547063149774&hydadcr=25614_9766669&keywords=atomic+habits+books&qid=1653348486&sr=8-1')
+scrapeProduct('https://www.amazon.ca/Atomic-Habits-Proven-Build-Break/dp/0735211299/ref=sr_1_1?gclid=CjwKCAjw4ayUBhA4EiwATWyBrqMPyRNjdAsm6qFAgPdgg3VU3V-49gSmfUbboklXEv5-jiFJXEw5hhoCVLEQAvD_BwE&hvadid=538107724032&hvdev=c&hvlocphy=9000800&hvnetw=g&hvqmt=e&hvrand=14293299909348710143&hvtargid=kwd-547063149774&hydadcr=25614_9766669&keywords=atomic+habits+books&qid=1653348486&sr=8-1');
