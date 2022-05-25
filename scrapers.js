9b8d717cd0fde7689d72332c76e08370229923c1
Joseph Tascona <jtascona@Josephs-MBP.hitronhub.home>
2022-05-25 15:56:06 -0400

    Made it work with wikipedia info

--- scrapers.js
+++ scrapers.js
@@ -1,27 +1,22 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeProduct(url) {
+async function scrapePage(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    await page.waitForXPath('//*[@id="imgBlkFront"]')
-    const [el] = await page.$x('//*[@id="imgBlkFront"]');
+    const [el] = await page.$x('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[2]/td/a/img');
     const src = el.getProperty('src');
     const imgURL = (await src).jsonValue();
 
-    await page.waitForXPath('//*[@id="productTitle"]')
-    const [el2] = await page.$x('//*[@id="productTitle"]');
+    const [el2] = await page.$x('//*[@id="firstHeading"]');
     const txt = el2.getProperty('textContent');
     const title = (await txt).jsonValue();
 
-    await page.waitForXPath('//*[@id="price"]')
-    const [el3] = await page.$x('//*[@id="price"]');
+    const [el3] = await page.$x('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[2]/td/div');
     const txt2 = el3.getProperty('textContent');
-    const price = (await txt2).jsonValue();
+    const value = (await txt2).jsonValue();
 
-    console.log({imgURL, title, price});
-    await browser.close();
+    console.log({imgURL, title, value});
 }
-
-scrapeProduct('https://www.amazon.ca/Atomic-Habits-Proven-Build-Break/dp/0735211299/ref=sr_1_1?gclid=CjwKCAjw4ayUBhA4EiwATWyBrqMPyRNjdAsm6qFAgPdgg3VU3V-49gSmfUbboklXEv5-jiFJXEw5hhoCVLEQAvD_BwE&hvadid=538107724032&hvdev=c&hvlocphy=9000800&hvnetw=g&hvqmt=e&hvrand=14293299909348710143&hvtargid=kwd-547063149774&hydadcr=25614_9766669&keywords=atomic+habits+books&qid=1653348486&sr=8-1');
+scrapePage('https://en.wikipedia.org/wiki/Julius_Caesar');
